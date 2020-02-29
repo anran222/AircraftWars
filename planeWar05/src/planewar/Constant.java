1f45afedda8b54ad345dac6fc9c3bf8317267b7a
anran222 <1432085874@qq.com>
2020-02-29 21:19:53 +0800

    存放相关常量

---
+++ planeWar05/src/planewar/Constant.java
@@ -0,0 +1,11 @@
+package planewar;
+
+/**
+ * @Author:xiang
+ * @Date:2020/2/29 18:39
+ * 存放相关常量
+ */
+public class Constant {
+    public static final int GAME_WIDTH=500;//窗口长度
+    public static final int GAME_HEIGHT=500;//窗口高度
+}
